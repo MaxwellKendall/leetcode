cdf4e90c3b9a2d865bc798d7069084d8dc771fe6
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2022-05-10 09:49:40 -0400

    is anagram

---
+++ arrays-and-hashing/is-anagram/index.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    const map = {};
+    const mapT = {}
+    if (s.length !== t.length) return false;
+    for (var i = 0; i < s.length; i++) {
+        const letter = s[i];
+        map[letter] = map[letter] ? map[letter] + 1: 1;
+    }
+    for (var i = 0; i < t.length; i++) {
+        const letter = t[i];
+        mapT[letter] = mapT[letter] ? mapT[letter] + 1: 1;
+    }
+    const letters = Object.keys(map);
+    for (var i = 0; i < letters.length; i++) {
+        const occurences = map[i];
+        const letter = letters[i];
+        const isBad = (
+            map[letter] !== mapT[letter]
+        )
+        if (isBad) {
+            return false;
+        }
+    }
+    return true;
+};
